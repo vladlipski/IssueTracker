9174e0f0832b28da40ae399c012e7c0f532fbebe
vladlipski <www.vlad.by55555@gmail.com>
2017-03-19 16:54:08 +0300

    add dao realization

--- src/main/java/by/issue_tracker/dao/DAO.java
+++ src/main/java/by/issue_tracker/dao/DAO.java
@@ -1,4 +1,32 @@
 package by.issue_tracker.dao;
 
-public class DAO {
+import by.issue_tracker.db.DbWorker;
+
+import java.sql.Connection;
+import java.util.List;
+
+public abstract class DAO<E, K> {
+    private DbWorker dbWorker;
+    private Connection connection;
+
+    public DAO() {
+        dbWorker = new DbWorker();
+        connection = dbWorker.getConnection();
+    }
+
+    public abstract List<E> getAll();
+    public abstract E update(E entity);
+    public abstract E getEntityById(K id);
+    public abstract boolean delete(K id);
+    public abstract boolean create(E entity);
+
+    public void closeConnection() {
+        dbWorker.closeConnection(connection);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        closeConnection();
+        super.finalize();
+    }
 }
---
+++ src/main/java/by/issue_tracker/dao/DaoFactory.java
@@ -0,0 +1,4 @@
+package by.issue_tracker.dao;
+
+public class DaoFactory {
+}
